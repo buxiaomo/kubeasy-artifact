2a923778d5a4c5837e7fcf5c04bc74bd2faea5d5
buxiaomo <95112082@qq.com>
2022-11-12 17:18:18 +0800

    no message

---
+++ version.py
@@ -0,0 +1,194 @@
+version_map = {
+    "1.14": {
+        "etcd": "3.3.10",
+        "cni": "0.7.5",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0",
+        },
+        "plugin": {
+            "coredns": "1.3.1",
+            "metrics": "0.5.2",
+            "pause": "3.1",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.15": {
+        "etcd": "3.3.10",
+        "cni": "0.7.5",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.3.1",
+            "metrics": "0.5.2",
+            "pause": "3.1",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.16": {
+        "etcd": "3.3.15",
+        "cni": "0.7.5",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.6.2",
+            "metrics": "0.5.2",
+            "pause": "3.1",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.17": {
+        "etcd": "3.4.3",
+        "cni": "0.7.5",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.6.5",
+            "metrics": "0.5.2",
+            "pause": "3.1",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.18": {
+        "etcd": "3.4.3",
+        "cni": "0.8.5",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.6.7",
+            "metrics": "0.5.2",
+            "pause": "3.2",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.19": {
+        "etcd": "3.4.13",
+        "cni": "0.8.6",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.7.0",
+            "metrics": "0.5.2",
+            "pause": "3.2",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.20": {
+        "etcd": "3.4.13",
+        "cni": "0.8.7",
+        "runtime": {
+            "docker": "19.03.9",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.7.0",
+            "metrics": "0.6.1",
+            "pause": "3.2",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.21": {
+        "etcd": "3.4.13",
+        "cni": "1.1.1",
+        "runtime": {
+            "docker": "20.10.17",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0"
+        },
+        "plugin": {
+            "coredns": "1.8.0",
+            "metrics": "0.6.1",
+            "pause": "3.4.1",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.22": {
+        "etcd": "3.5.0",
+        "cni": "1.1.1",
+        "runtime": {
+            "docker": "20.10.17",
+            "containerd": "1.5.13",
+            "runc": "1.1.2",
+            "crictl": "1.20.0",
+        },
+        "plugin": {
+            "coredns": "1.8.4",
+            "metrics": "0.6.1",
+            "pause": "3.5",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.23": {
+        "etcd": "3.5.1",
+        "cni": "1.1.1",
+        "runtime": {
+            "docker": "20.10.17",
+            "containerd": "1.6.8",
+            "runc": "1.1.3",
+            "crictl": "1.24.1"
+        },
+        "plugin": {
+            "coredns": "1.8.6",
+            "metrics": "0.6.1",
+            "pause": "3.6",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.24": {
+        "etcd": "3.5.3",
+        "cni": "1.1.1",
+        "runtime": {
+            "docker": "20.10.17",
+            "containerd": "1.6.8",
+            "runc": "1.1.3",
+            "crictl": "1.24.1"
+        },
+        "plugin": {
+            "coredns": "1.8.6",
+            "metrics": "0.6.1",
+            "pause": "3.6",
+            "nvidia": "0.12.3"
+        }
+    },
+    "1.25": {
+        "etcd": "3.5.4",
+        "cni": "1.1.1",
+        "runtime": {
+            "docker": "20.10.17",
+            "containerd": "1.6.8",
+            "runc": "1.1.3",
+            "crictl": "1.24.1"
+        },
+        "plugin": {
+            "coredns": "1.9.3",
+            "metrics": "0.6.1",
+            "pause": "3.8",
+            "nvidia": "0.12.3"
+        }
+    }
+}
